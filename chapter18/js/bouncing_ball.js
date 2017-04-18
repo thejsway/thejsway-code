3137835c127b58a2ec11490d23e473ee4f80efcb
bpesquet <bpesquet@gmail.com>
2017-04-18 17:45:40 +0200

    Chapter 18 exercises

---
+++ chapter18/js/bouncing_ball.js
@@ -0,0 +1,52 @@
+/*
+Bouncing ball
+*/
+
+const frameElement = document.getElementById("frame");
+const ballElement = document.getElementById("ball");
+// Movement value in pixels
+const movement = 7; 
+// Convert ball diameter (value of the form "XXpx") to a number
+const ballDiameter = parseFloat(getComputedStyle(ballElement).width);
+// Animation ID - Global variable to access it from functions
+let animationId = null;
+// Minimal left position (left border)
+const xMin = 0;
+// Direction info - 1=right, 2=left
+let direction = 1;
+
+// Déplace le ballElement vers la gauche ou la droite
+function animateBall() {
+    // Convert the left position of the ball (value of the form "XXpx") to a number
+    const xBall = parseFloat(getComputedStyle(ballElement).left);
+    // Convert the width of the frame (value of the form "XXpx") to a number
+    const xMax = parseFloat(getComputedStyle(frameElement).width);
+    // If the ball is at an adge of the frame
+    if ((xBall + ballDiameter > xMax) || (xBall < xMin)) {
+        // Change direction
+        direction *= -1;
+    }
+    // Move the ball in the current direction
+    ballElement.style.left = (xBall + movement * direction) + "px";
+    // Request another animation
+    animationId = requestAnimationFrame(animateBall);
+}
+
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+
+startButton.addEventListener("click", () => {
+    // Change button states
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    // Start the animation
+    animationId = requestAnimationFrame(animateBall);
+});
+
+stopButton.addEventListener("click", () => {
+    // Change button states
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    // Stop animation
+    cancelAnimationFrame(animationId);
+});
---
+++ chapter18/js/chronometer.js
@@ -0,0 +1,30 @@
+/*
+Chronometer
+*/
+
+const buttonElement = document.querySelector("button");
+const counterElement = document.getElementById("counter");
+// Global variable to access it from function
+let intervalId = null;
+// Chronometer state, initially stopped
+let started = false;
+
+
+buttonElement.addEventListener("click", () => {
+  if (!started) {
+    // Start the chronometer: add 1 to the counter each second
+    intervalId = setInterval(() => {
+      counterElement.textContent = Number(counterElement.textContent) + 1;
+    }, 1000);
+    // Update button text
+    buttonElement.textContent = "Stop";
+  } 
+  else {
+    // Stop the chronometer
+    clearInterval(intervalId);
+    // Update button text
+    buttonElement.textContent = "Start";
+  }
+  // Change chronometer state
+  started = !started;
+});
