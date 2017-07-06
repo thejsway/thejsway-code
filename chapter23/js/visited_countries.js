c716e67615b4f2f1a17bf5d947bfac12762a1783
bpesquet <bpesquet@gmail.com>
2017-07-06 11:46:13 +0200

    Update chapter 23 exercises

---
+++ chapter23/js/visited_countries.js
@@ -0,0 +1,39 @@
+/*
+Send visited coutnries
+*/
+
+// Create traveler info
+const traveler = {
+  name: "Sam",
+  countries: [
+    {
+      name: "Ecuador",
+      year: 2003
+    },
+    {
+      model: "Portugal",
+      color: 2004
+    },
+    {
+      model: "Russia",
+      color: 2016
+    }
+  ]
+};
+
+// Send this object as JSON data to the server
+fetch("https://thejsway.glitch.me/api/countries", {
+  method: "POST",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(traveler)
+})
+  .then(response => response.text())
+  .then(result => {
+    console.log(result);
+  })
+  .catch(err => {
+    console.error(err.message);
+  });
